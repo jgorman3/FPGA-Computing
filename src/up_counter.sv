70be92d030af26e92a157480275bbc2bdc0fd004
Joe Gorman <jgorman3@lion.lmu.edu>
2018-08-14 11:14:22 -0400

    created up_counter.sv
    quickly realizing v and sv are pretty similar

---
+++ src/up_counter.sv
@@ -0,0 +1,14 @@
+module up_counter(out, enable, clk, reset);
+
+output reg [7:0] out;
+input  wire enable; 
+input  wire clk;
+input  wire reset;
+
+always_ff @(posedge clk)
+if (reset) begin
+  out <= 8'b0 ;
+end else if (enable) begin
+   out ++;
+end
+endmodule
